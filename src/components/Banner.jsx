7e4671d6341fc969a719af4b1fab8259a47bc068
Rustambek <rustambekrustamov180@gmail.com>
2026-06-18 22:53:30 +0500

    added banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 
 function Banner() {
   const [banners, setBanners] = useState([])
-  const [swiper, setSwiper] = useState(null)
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     fetch("https://6c17cdd241beda19.mokky.dev/banners").then((request) =>
@@ -10,16 +10,47 @@ function Banner() {
     )
   }, [])
 
+  const goNext = () => {
+    if (currentIndex === banners.length - 1) {
+      setCurrentIndex(0)
+    } else {
+      setCurrentIndex(currentIndex + 1)
+    }
+  }
+
+  const goBack = () => {
+    if (currentIndex === 0) {
+      setCurrentIndex(banners.length - 1)
+    } else {
+      setCurrentIndex(currentIndex - 1)
+    }
+  }
+
   return (
-    <div className="max-lg:aspect-10/3 aspect-12/3 flex overflow-hidden rounded-3xl">
+    <div className="relative max-lg:aspect-10/3 aspect-12/3 flex overflow-hidden rounded-3xl">
+      <button
+        onClick={() => {
+          goBack()
+        }}
+        className="flex absolute cursor-pointer left-2 top-1/2 -translate-y-1/2"
+      >
+        left
+      </button>
       {banners.length > 0 && (
         <img
           className="object-cover w-full rounded-3xl"
-          src={banners[0].img}
-          key={banners[0].id}
+          src={banners[currentIndex].img}
           alt=""
         />
       )}
+      <button
+        onClick={() => {
+          goNext()
+        }}
+        className="flex absolute cursor-pointer right-2 top-1/2 -translate-y-1/2"
+      >
+        right
+      </button>
     </div>
   )
 }
